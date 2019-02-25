5c6d9818245ac353bc059afcb0dcf1f191e22706
Sean Gray <Sean@Seans-MacBook-Pro.local>
2019-02-25 16:54:05 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
+console.log("Up and Running!");
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
 
-if (cardsInPlay === 2) {
-	cardsInPlay.length == 2;
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("you found a match");
-} else if (cardsInPlay[0] !== cardsInPlay[2]) {
+	} else {
 	alert("Sorry try again!!");
-};
+	}
+}
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
