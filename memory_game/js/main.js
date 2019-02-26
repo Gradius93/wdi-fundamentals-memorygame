ed0588e2989f7a681e28ba47e7b132b9be37603f
Sean Gray <sean.a.gray@hotmail.co.uk>
2019-02-26 19:09:23 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,32 @@
 console.log("Up and Running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{	
+suit: "Hearts",
+rank: "Queen",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+suit: "Hearts",
+rank: "King",
+cardImage: "images/king-of-hearts.png"
+},
+{
+suit: "Diamonds",
+rank: "Queen",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+suit: "Diamonds",
+rank: "King",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
 	alert("you found a match");
 	} else {
 	alert("Sorry try again!!");
@@ -12,9 +34,11 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
